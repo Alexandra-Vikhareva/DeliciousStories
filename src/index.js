1d7e64ad0c2d7514a0b46c9b2286633928b0bd1f
Sasha <st087955@student.spbu.ru>
2025-01-22 14:41:13 +0300

    Импорнтированы картинки для Главной страницы

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import "./styles.css";
 import { createCard } from "./home";
 import { menu, descriptionMenu, Food } from "./menu";
 import { infoAbout, hours, contacts } from "./about";
+import community from './community.jpg';
+import interior from './interior.jpg';
+import specialEvents from './specialEvents.jpg';
 
 function drawHome(){
     const cards = document.createElement('div');
@@ -9,9 +12,9 @@ function drawHome(){
     const description = document.createElement('div');
     description.className = 'description';
     description.textContent = 'Ресторан "Вкусные истории" предлагает уникальный опыт, позволяя гостям не только насладиться вкусной едой, но и узнать больше о культуре и традициях разных стран. Меню разнообразно и включает в себя как классические, так и современные блюда, что делает его привлекательным для широкой аудитории.';
-    cards.append(createCard('Тематика', 'Ресторан посвящен кулинарным традициям разных стран, предлагая блюда, которые рассказывают истории о культуре и кухне этих стран.', './interior.jpg'));
-    cards.append(createCard('Целевая аудитория', 'Ресторан ориентирован на любителей гастрономических путешествий: семьи, пары и туристы, желающие попробовать что-то новое и необычное.', './community.jpg'));
-    cards.append(createCard('Специальные мероприятия', 'Регулярные кулинарные мастер-классы, дегустации вин и тематические вечера, посвященные определенной стране или кухне.', './specialEvents.jpg'));
+    cards.append(createCard('Тематика', 'Ресторан посвящен кулинарным традициям разных стран, предлагая блюда, которые рассказывают истории о культуре и кухне этих стран.', interior));
+    cards.append(createCard('Целевая аудитория', 'Ресторан ориентирован на любителей гастрономических путешествий: семьи, пары и туристы, желающие попробовать что-то новое и необычное.', community));
+    cards.append(createCard('Специальные мероприятия', 'Регулярные кулинарные мастер-классы, дегустации вин и тематические вечера, посвященные определенной стране или кухне.', specialEvents));
 
     const content = document.querySelector('#content');
     content.append(description);
